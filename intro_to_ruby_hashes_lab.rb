6f337fb2ff07992d2bce998f59b24e365073fb32
burbage stacey <rstacey06@gmail.com>
2019-10-23 17:26:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
     railroads: {
       pieces: 4,
       names: {
-        :reading_railroad => {"morga"},
+        :reading_railroad => {"morgage_v"},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
